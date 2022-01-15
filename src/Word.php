3520459fcf99ae5049b6efd2def0465683807e80
maxi <maxi@conversalab.com>
2022-01-15 19:17:53 -0300

    Word test06 refactor

--- src/Word.php
+++ src/Word.php
@@ -5,10 +5,8 @@ Namespace Wordle;
 final class Word {
 
     function __construct(string $letters) {
-        if (str_contains($letters, '.'))
+        if (!\preg_match('/^[a-z]+$/i', $letters)) {
             throw new \Exception('word contain invalid letters');
-        if (str_contains($letters,'*')) {
-           throw new \Exception('word contain invalid letters');
         }
         if (strlen($letters) < 5) {
             throw new \Exception('Too few letters. Should be 5');
